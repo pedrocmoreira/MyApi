d1eceee0486effe33146d12e18ac78c13419fea4
Pedro Moreira <pedro.henrique@agenciaalldigital.com>
2022-12-19 17:18:44 -0300

    chore: adicionando erro para api

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
+import { AppError } from '@shared/errors/AppError';
 import { routes } from './routes';
 
 const app = express();
@@ -11,6 +12,22 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    console.log(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  }
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}`);
