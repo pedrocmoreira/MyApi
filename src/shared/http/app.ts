bab6924f17cb2f406e29d91c98de0a499f23f84b
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-12-20 16:59:50 -0300

    feat: adicionando typeorm e sqlite ao projeto

---
+++ src/shared/http/app.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from '../../swagger.json';
+
+import { AppError } from '@shared/errors/AppError';
+import { routes } from './routes';
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+  console.log(error);
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+export { app };
