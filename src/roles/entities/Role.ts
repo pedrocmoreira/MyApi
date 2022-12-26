3415cf92ebfe9166ffa199079c78097ace027b1e
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-12-26 10:17:20 -0300

    refactor: role entity

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -1,8 +1,15 @@
 import { randomUUID } from 'node:crypto';
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 
+@Entity('roles')
 export class Role {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @CreateDateColumn()
   created_at: Date;
 
   constructor() {
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
+import { Role } from '@roles/entities/Role';
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1671566877269 } from './migrations/1671566877269-CreateRolesTable';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1671566877269],
 });
