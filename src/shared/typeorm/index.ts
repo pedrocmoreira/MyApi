a2b073cca08718ce90b47f387d77717b45f694d8
Pedro Moreira <pedro.henrique@agenciaalldigital.com>
2022-12-27 14:12:23 -0300

    chore: criando migration para adicionar Role para user

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -2,10 +2,15 @@ import { Role } from '@roles/entities/Role';
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1671566877269 } from './migrations/1671566877269-CreateRolesTable';
 import { CreateUsersTable1672160209574 } from './migrations/1672160209574-CreateUsersTable';
+import { AddRoleIdToUsersTable1672160736069 } from './migrations/1672160736069-AddRoleIdToUsersTable';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1671566877269, CreateUsersTable1672160209574],
+  migrations: [
+    CreateRolesTable1671566877269,
+    CreateUsersTable1672160209574,
+    AddRoleIdToUsersTable1672160736069,
+  ],
 });
