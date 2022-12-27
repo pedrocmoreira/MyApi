cd19bd5f43f097a563a15c5cd9051d0c90949219
Pedro Moreira <pedro.henrique@agenciaalldigital.com>
2022-12-27 14:04:03 -0300

    chore: adicionando e criando migration da tabela users

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,10 +1,11 @@
 import { Role } from '@roles/entities/Role';
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1671566877269 } from './migrations/1671566877269-CreateRolesTable';
+import { CreateUsersTable1672160209574 } from './migrations/1672160209574-CreateUsersTable';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [Role],
-  migrations: [CreateRolesTable1671566877269],
+  migrations: [CreateRolesTable1671566877269, CreateUsersTable1672160209574],
 });
