92fe6ec41732be6a18628374495816144e86e245
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-12-28 16:23:29 -0300

    feat: adicionando middleware para autenticação de usuário com token

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -7,6 +7,7 @@ import { ListRolesController } from '@roles/useCases/listRoles/ListRolesControll
 import { ShowRoleController } from '@roles/useCases/showRole/ShowRoleController';
 import { UpdateRoleController } from '@roles/useCases/updateRole/UpdateRoleController';
 import { DeleteRoleController } from '@roles/useCases/deleteRole/DeleteRoleController';
+import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated';
 
 const rolesRouter = Router();
 
@@ -15,6 +16,8 @@ const listRolesController = container.resolve(ListRolesController);
 const showRolesController = container.resolve(ShowRoleController);
 const updateRolesController = container.resolve(UpdateRoleController);
 const deleteRolesController = container.resolve(DeleteRoleController);
+
+rolesRouter.use(isAuthenticated);
 
 rolesRouter.post(
   '/',
