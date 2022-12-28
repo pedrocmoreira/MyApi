fd3713c1a6e527b8889c00934352e49f1376429d
Pedro Moreira <pedrocmoreira97@gmail.com>
2022-12-28 17:13:28 -0300

    feat: criado UpdateAvatarUseCase

---
+++ src/users/useCases/updateAvatar/UpdateAvatarUseCase.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from 'tsyringe';
+import path from 'node:path';
+import fs from 'node:fs';
+
+import { AppError } from '@shared/errors/AppError';
+import { User } from '@users/entities/User';
+
+import { IUsersRepository } from '@users/repositories/IUsersRepository';
+import uploadConfig from '../../../config/upload';
+
+export type UpdateAvatarDTO = {
+  userId: string;
+  avatarFileName: string;
+};
+
+@injectable()
+export class UpdateAvatarUseCase {
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ avatarFileName, userId }: UpdateAvatarDTO): Promise<User> {
+    const user = await this.usersRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError('Only authentcated users can change avatar', 401);
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+
+      user.avatar = avatarFileName;
+      return await this.usersRepository.save(user);
+    }
+  }
+}
