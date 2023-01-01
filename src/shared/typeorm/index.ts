b02c7e98fb20d5a1cb83c53c5dbacbc6d11098fc
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-01 17:49:35 -0300

    feat: creiando tabelas para refresh_tokens

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,6 +4,7 @@ import { DataSource } from 'typeorm';
 import { CreateRolesTable1671566877269 } from './migrations/1671566877269-CreateRolesTable';
 import { CreateUsersTable1672160209574 } from './migrations/1672160209574-CreateUsersTable';
 import { AddRoleIdToUsersTable1672160736069 } from './migrations/1672160736069-AddRoleIdToUsersTable';
+import { CreateRefreshTokensTable1672605682627 } from './migrations/1672605682627-CreateRefreshTokensTable';
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
@@ -13,5 +14,6 @@ export const AppDataSource = new DataSource({
     CreateRolesTable1671566877269,
     CreateUsersTable1672160209574,
     AddRoleIdToUsersTable1672160736069,
+    CreateRefreshTokensTable1672605682627,
   ],
 });
